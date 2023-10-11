776035063e3477e0b182d8df41d1ca66745b043c
fgaujard <flavien.gaujard@gmail.com>
2023-10-11 11:52:49 +0200

    trololol React

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,9 +1,27 @@
+import styles from './Pokemon.css'
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+
+  const pokemon = pokemonList[1];
+    
   return (
     <>
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
+        <div>
+        <figcaption>{pokemon.name}</figcaption>
+        </div>
       </figure>
     </>
   )
