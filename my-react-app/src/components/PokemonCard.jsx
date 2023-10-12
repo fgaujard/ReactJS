ebc3b27b4dc81068b8a0ea499a17496a7fa44837
fgaujard <flavien.gaujard@gmail.com>
2023-10-12 10:06:37 +0200

    propsReact

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,27 +1,11 @@
 import styles from './Pokemon.css'
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-
-  const pokemon = pokemonList[1];
-    
+function PokemonCard({pokemon}) {
   return (
     <>
       <figure>
         {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
-        <div>
         <figcaption>{pokemon.name}</figcaption>
-        </div>
       </figure>
     </>
   )
